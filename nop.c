98ae73d13c72029ee3d45b6486b53ce907033ba5
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-28 00:30:25 +0300

    task 5

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- don,t do any thing
+  *@head: pointer  to atack
+  *@line_number: number of line.
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int line_number)
+{
+	(void) line_number;
+	(void) head;
+}
