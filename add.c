797dd1ae18a2e45390ae1bb82ba66826585730f1
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-28 00:24:32 +0300

    task 4

---
+++ add.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * f_add - adds the top two elements of the stack.
+ * @head: pointer to the stack
+ * @line_number: line number
+*/
+void f_add(stack_t **head, unsigned int line_number)
+{
+	stack_t *p;
+	int count = 0, temp;
+
+	p = *head;
+	while (p)
+	{
+		p = p->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fclose(info_global.file);
+		free(info_global.buffer);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	p = *head;
+	temp = p->n;
+	p->next->n += temp;
+	*head = (*head)->next;
+	free(p);
+}
