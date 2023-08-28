1e419a4d018cd71d1fcbc49042ba8aa841c9dd43
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-28 04:41:19 +0300

    task 8

--- monty.h
+++ monty.h
@@ -72,6 +72,7 @@ void f_add(stack_t **head, unsigned int line_number);
 void f_nop(stack_t **head, unsigned int line_number);
 void f_sub(stack_t **head, unsigned int line_number);
 void f_div(stack_t **head, unsigned int line_number);
+void f_mul(stack_t **head, unsigned int line_number);
 /*-----------------------------*/
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
